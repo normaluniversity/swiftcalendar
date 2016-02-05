7c1b560ec78ad98078c4b64ae4a27b289d635930
normaluniversity <normaluniversity@gmail.com>
2016-02-05 12:01:02 +0900

    Create CalendarMain.swift

---
+++ CalendarMain.swift
@@ -0,0 +1,22 @@
+//
+//  CalendarMain.swift
+//  SwiftCalendar
+//
+//  Created by wuge on 2016/02/04.
+//  Copyright © 2016年 Yubun. All rights reserved.
+//
+//* This code includes "sun_position.m" by Khalil Sultan
+//* http://www.mathworks.com/matlabcentral/fileexchange/5430-sun-azimuth-data
+//* Copyright 08 Jul 2004 Khalil Sultan (Updated 09 Jul 2004 MATLAB Central )
+//* Released under the BSD Licenses.
+//* MATLAB release	MATLAB 6.5 (R13)
+//*
+//* This code includes "moonpos.pro" by by Jean Meeus (1998)
+//* http://www.rdocumentation.org/packages/astrolibR/functions/moonpos
+//* The method is derived from the Chapront ELP2000/82 Lunar Theory (Chapront-Touze and Chapront, 1983)
+//* Written W. Landsman R adaptation by Arnab Chakraborty June 2013
+//* Released under the GPL Licenses.
+
+class CalendarMain{
+
+}
